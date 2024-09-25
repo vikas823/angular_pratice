fe412ba36602f95ba179ffb6cdaa6178244af313
Sai Vikas Addanki <saivikasaddanki@Sais-MacBook-Air.local>
2024-09-25 17:39:47 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent,NavbarComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
